1a24bf25af902dc97fcbd9711e4fe478a2f31aeb
Utkarsh Patel <iamutkarsh@live.com>
2016-01-21 02:26:24 +0530

    More functions for nonce and their test cases added

--- src/wp-nonce-wrapper.php
+++ src/wp-nonce-wrapper.php
@@ -72,5 +72,46 @@ if ( ! class_exists( 'Wp_Nonce_Wrapper' ) ) :
 			return wp_verify_nonce( $nonce, $action );
 		}
 
+		/**
+		 * Create nonce field
+		 *
+		 * @param int|string $action Optional. Action name. Default -1.
+		 * @param string $name Optional. Nonce name. Default '_wpnonce'.
+		 * @param bool $referer Optional. Whether to set the referer field for validation. Default true.
+		 * @param bool $echo Optional. Whether to display or return hidden form field. Default true.
+		 *
+		 * @return string Nonce field HTML markup.
+		 */
+		public function create_nonce_field( $action = - 1, $name = "_wpnonce", $referer = true, $echo = true ) {
+			return wp_nonce_field( $action, $name, $referer, $echo );
+		}
+
+		/**
+		 * Get nonce url
+		 *
+		 * @param string $actionurl URL to add nonce action.
+		 * @param int|string $action Optional. Nonce action name. Default -1.
+		 * @param string $name Optional. Nonce name. Default '_wpnonce'.
+		 *
+		 * @return string Escaped URL with nonce action added.
+		 */
+		public function create_nonce_url( $actionurl, $action = - 1, $name = '_wpnonce' ) {
+			return wp_nonce_url( $actionurl, $action, $name );
+		}
+
+		/**
+		 * Check admin referral
+		 *
+		 * @param int|string $action Action nonce.
+		 * @param string $query_arg Optional. Key to check for nonce in `$_REQUEST` (since 2.5).
+		 *                              Default '_wpnonce'.
+		 *
+		 * @return false|int False if the nonce is invalid, 1 if the nonce is valid and generated between
+		 *                   0-12 hours ago, 2 if the nonce is valid and generated between 12-24 hours ago.
+		 */
+		public function check_admin_referral( $action = - 1, $query_arg = '_wpnonce' ) {
+			return check_admin_referer( $action, $query_arg );
+		}
+
 	}
 endif;
--- tests/test-wp-nonce-wrapper.php
+++ tests/test-wp-nonce-wrapper.php
@@ -6,6 +6,7 @@ class test_wp_nonce_wrapper extends WP_UnitTestCase {
 
 	/**
 	 * test_wp_nonce_wrapper constructor.
+	 * Initialize Wp_Nonce_Wrapper
 	 */
 	public function __construct() {
 		$this->instance = Wp_Nonce_Wrapper::getInstance();
@@ -15,11 +16,49 @@ class test_wp_nonce_wrapper extends WP_UnitTestCase {
 		// replace this with some actual testing code
 		$nonce = $this->instance->create_nonce( "doing_some_form_job" );
 		$this->assertEquals( $this->instance->verify_nonce( $nonce, "doing_some_form_job" ), 1 );
+		$this->assertNotEquals( $this->instance->verify_nonce( $nonce . 'd', "doing_some_form_job" ), 1 );
 	}
 
-	function test_nonce_logged_in(){
+	function test_nonce_logged_in() {
+		//set admin as a user
 		wp_set_current_user( 1 );
 		$nonce = $this->instance->create_nonce( "some_awesome_action" );
+		// verify nonce
 		$this->assertEquals( $this->instance->verify_nonce( $nonce, "some_awesome_action" ), 1 );
+
+		// check if wrong nonce inserted
+		$this->assertNotEquals( $this->instance->verify_nonce( $nonce . 'd', "some_awesome_action" ), 1 );
+	}
+
+	function test_create_nonce_field() {
+		//get html field
+		$html_input_field = $this->instance->create_nonce_field( 'clean_field', '_wpnonce', true, false );
+		$dom              = new DOMDocument();
+		//Load html field to get value from it.
+		$dom->loadHTML( $html_input_field );
+		$inputs = $dom->getElementsByTagName( 'input' );
+		if ( ! empty( $inputs[0] ) ) {
+			$nonce = $inputs[0]->getAttribute( 'value' );
+			//verify
+			$this->assertEquals( $this->instance->verify_nonce( $nonce, 'clean_field' ), 1 );
+		}
+	}
+
+	function test_create_nonce_url() {
+		// get url with nonce field
+		$url   = $this->instance->create_nonce_url( "http://w.org", 'clean_url' );
+		$query = parse_url( $url );
+		$query = $query['query'];
+		// parse url and get query from it and extract nonce
+		$nonce = substr( $query, ( strpos( $query, '=' ) + 1 ) );
+		//verify nonce
+		$this->assertEquals( $this->instance->verify_nonce( $nonce, 'clean_url' ), 1 );
+	}
+
+	function test_check_admin_referral() {
+		$nonce                = $this->instance->create_nonce( "doing_some_admin_job" );
+		$_REQUEST['_wpnonce'] = $nonce;
+		//check admin referral for verifying nonce
+		$this->assertEquals( $this->instance->check_admin_referral( 'doing_some_admin_job' ), 1 );
 	}
 }
