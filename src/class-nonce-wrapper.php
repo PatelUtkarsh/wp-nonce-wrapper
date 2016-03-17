a478f2a2b989796791af06d07ee228a02ee79798
Utkarsh Patel <iamutkarsh@live.com>
2016-03-18 01:18:37 +0530

    Remove php magic method  and refactor code

--- src/class-nonce-wrapper.php
+++ src/class-nonce-wrapper.php
@@ -1,18 +1,25 @@
-<?php
+<?php # -*- coding: utf-8 -*-
 
 namespace spock\helper;
 
 if ( ! class_exists( 'Nonce_Wrapper' ) ) :
 	/**
 	 * Class Nonce_Wrapper
+	 *
+	 * Simplified nonce usage
+	 *
+	 * @package spock\helper
 	 */
 	class Nonce_Wrapper {
 
 		/**
-		 * @var Nonce_Wrapper Action that is being performed.
+		 * @var string $action that is being performed.
 		 */
 		private $action;
 
+		/**
+		 * @var int $nonce expire time
+		 */
 		private $expire;
 
 		/**
@@ -26,13 +33,15 @@ if ( ! class_exists( 'Nonce_Wrapper' ) ) :
 			$this->expire = $expire;
 		}
 
+		/****************************************************
+		 ***************  Getters  **************************
+		 ****************************************************/
+
 		/**
-		 *
-		 * @param $expire
-		 *
+		 * Get expire value
 		 * @return int
 		 */
-		function set_expire( $expire ) {
+		public function get_expire() {
 			return $this->expire;
 		}
 
@@ -43,25 +52,62 @@ if ( ! class_exists( 'Nonce_Wrapper' ) ) :
 			return $this->action;
 		}
 
+		/****************************************************
+		 ********************* Hooks ************************
+		 *****************************************************/
+
+		/**
+		 * Hook call back that sets given Expire
+		 *
+		 * @param $expire
+		 *
+		 * @return int
+		 */
+		function set_expire( $expire ) {
+			return $this->get_expire();
+		}
+
+		/**
+		 * Check if expire is same as default WordPress Expire in that case we don't need to add filter
+		 * @return bool
+		 */
+		private function is_hookable()  {
+			return ( 86400 !== $this->get_expire() );
+		}
+
+		/**
+		 * Set hook to nonce_life is is_hookable
+		 */
+		protected function set_hook() {
+			if ( $this->is_hookable() ) {
+				\add_filter( 'nonce_life', array( $this, 'set_expire' ), 1 );
+			}
+		}
+
+		/**
+		 * Remove hook from `nonce_life` after nonce is generated or verified in case of is hookable
+		 */
+		protected function remove_hook() {
+			if ( $this->is_hookable() ) {
+				\remove_filter( 'nonce_life', array( $this, 'set_expire' ), 1 );
+			}
+		}
+
+		/****************************************************
+		 ******************* Nonce methods ******************
+		 *****************************************************/
 		/**
 		 * Create nonce
 		 * @return string
 		 *
 		 */
-		protected function create_nonce() {
-			return \wp_create_nonce( $this->action );
+		public function create_nonce() {
+			$this->set_hook();
+			$nonce = \wp_create_nonce( $this->action );
+			$this->remove_hook();
+
+			return $nonce;
 		}
-
-		public function __call( $name, $arguments ) {
-			if ( method_exists( $this, $name ) ) {
-				add_filter( 'nonce_life', array( $this, 'set_expire' ), 1 );
-				$return = call_user_func_array( array( $this, $name ), $arguments );
-				remove_filter( 'nonce_life', array( $this, 'set_expire' ), 1 );
-
-				return $return;
-			}
-		}
-
 
 		/**
 		 * Verify nonce
@@ -70,8 +116,12 @@ if ( ! class_exists( 'Nonce_Wrapper' ) ) :
 		 *
 		 * @return false|int
 		 */
-		protected function verify_nonce( $nonce ) {
-			return \wp_verify_nonce( $nonce, $this->get_action() );
+		public function verify_nonce( $nonce ) {
+			$this->set_hook();
+			$is_valid_nonce = \wp_verify_nonce( $nonce, $this->get_action() );
+			$this->remove_hook();
+
+			return $is_valid_nonce;
 		}
 
 		/**
@@ -83,8 +133,12 @@ if ( ! class_exists( 'Nonce_Wrapper' ) ) :
 		 *
 		 * @return string Nonce field HTML markup.
 		 */
-		protected function create_nonce_field( $name = '_wpnonce', $referer = true, $echo = true ) {
-			return \wp_nonce_field( $this->get_action(), $name, $referer, $echo );
+		public function create_nonce_field( $name = '_wpnonce', $referer = true, $echo = true ) {
+			$this->set_hook();
+			$nonce_field = \wp_nonce_field( $this->get_action(), $name, $referer, $echo );
+			$this->remove_hook();
+
+			return $nonce_field;
 		}
 
 		/**
@@ -95,8 +149,12 @@ if ( ! class_exists( 'Nonce_Wrapper' ) ) :
 		 *
 		 * @return string Escaped URL with nonce action added.
 		 */
-		protected function create_nonce_url( $actionurl, $name = '_wpnonce' ) {
-			return \wp_nonce_url( $actionurl, $this->get_action(), $name );
+		public function create_nonce_url( $actionurl, $name = '_wpnonce' ) {
+			$this->set_hook();
+			$nonce_url = \wp_nonce_url( $actionurl, $this->get_action(), $name );
+			$this->remove_hook();
+
+			return $nonce_url;
 		}
 
 		/**
@@ -108,8 +166,12 @@ if ( ! class_exists( 'Nonce_Wrapper' ) ) :
 		 * @return false|int False if the nonce is invalid, 1 if the nonce is valid and generated between
 		 *                   0-12 hours ago, 2 if the nonce is valid and generated between 12-24 hours ago.
 		 */
-		protected function check_admin_referral( $query_arg = '_wpnonce' ) {
-			return \check_admin_referer( $this->get_action(), $query_arg );
+		public function check_admin_referral( $query_arg = '_wpnonce' ) {
+			$this->set_hook();
+			$is_valid_admin_referer = \check_admin_referer( $this->get_action(), $query_arg );
+			$this->remove_hook();
+
+			return $is_valid_admin_referer;
 		}
 
 		/**
@@ -120,23 +182,27 @@ if ( ! class_exists( 'Nonce_Wrapper' ) ) :
 		 *
 		 * @return false|int
 		 */
-		protected function check_ajax_referer( $query_arg = false, $die = true ) {
-			return \check_ajax_referer( $this->get_action(), $query_arg, $die );
+		public function check_ajax_referer( $query_arg = false, $die = true ) {
+			$this->set_hook();
+			$is_valid_ajax_reference = \check_ajax_referer( $this->get_action(), $query_arg, $die );
+			$this->remove_hook();
+
+			return $is_valid_ajax_reference;
 		}
 
 		/**
 		 * Display 'Are You Sure' message to confirm the action being taken.
 		 */
-		protected function nonce_ays() {
+		public function nonce_ays() {
 			\wp_nonce_ays( $this->get_action() );
 		}
 
 		/**
 		 * Get the time-dependent variable for nonce creation.
-		 * @return integer
+		 * @return int
 		 */
-		protected function nonce_tick() {
-			return wp_nonce_tick();
+		public function nonce_tick() {
+			return \wp_nonce_tick();
 		}
 
 	}
